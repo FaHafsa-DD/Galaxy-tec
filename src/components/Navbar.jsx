f6a900166de5821848816bf3c09467189e891fc5
Hafsa <hafsahaniffa1@gmail.com>
2025-11-17 21:00:47 +0530

    Second commit: changed the primary colour and modified the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar({ theme, setTheme }) {
   return (
-    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
-      <h1 color={theme === "dark" ? "#fff" : "#000"}>Galaxy-Tech</h1>
+    <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70 my-4">
+      <h1 className="dark:text-white text-black font-extrabold text-4xl">Galaxy<span className="italic text-primary font-bold">tech</span></h1>
     </div>
   );
 }
